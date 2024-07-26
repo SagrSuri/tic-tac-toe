e920dfae24f98e0ee69fecc343226fbfe69e3e9d
SAGRSURI <sagarsuriofficial@gmail.com>
2024-07-26 19:50:41 +0530

    V.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Card from './Components/Card/Card'
+import Grid from './Components/Grid/Grid'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <Grid numberOfCards={9} />
+    </>
+  )
+}
+
+export default App
